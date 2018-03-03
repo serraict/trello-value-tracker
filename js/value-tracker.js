3fc0cb3ba15cb30494822e3da4da079b0e8ccc46
Marijn van der Zee <marijn@serraict.com>
2018-03-03 10:17:38 +0100

    Access all data on the card

--- js/value-tracker.js
+++ js/value-tracker.js
@@ -3,9 +3,10 @@
 var t = TrelloPowerUp.iframe();
 
 t.render(function(){
-  return t.get('card', 'shared', 'value-for-me')
-  .then(function(value_for_me){
-    window.value_for_me.value = value_for_me;
+  return t.get('card', 'shared')
+  .then(function(data){
+    console.log(data);
+    window.value_for_me.value = data['value_for_me'];
   })
   .then(function(){
     t.sizeTo('#value_form').done();
@@ -15,7 +16,7 @@ t.render(function(){
 window.value_form.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-  return t.set('card', 'shared', 'value-for-me', window.value_for_me.value)
+  return t.set('card', 'shared', 'value_for_me', window.value_for_me.value)
   .then(function(){
     t.closePopup();
   });

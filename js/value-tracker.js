1cc80fc5535b24506af14495c610c01841b06054
Marijn van der Zee <marijn@serraict.com>
2018-03-03 10:30:06 +0100

    Set value for others too

--- js/value-tracker.js
+++ js/value-tracker.js
@@ -6,7 +6,8 @@ t.render(function(){
   return t.get('card', 'shared')
   .then(function(data){
     console.log(data);
-    window.value_for_me.value = data['value_for_me'];
+    window.value_for_me.value = data.value_for_me;
+    window.value_for_others.value = data.value_for_others;
   })
   .then(function(){
     t.sizeTo('#value_form').done();
@@ -17,7 +18,8 @@ window.value_form.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
   return t.set('card', 'shared', 'value_for_me', window.value_for_me.value)
+  .then(t.set('card', 'shared', 'value_for_others', window.value_for_others.value)
   .then(function(){
     t.closePopup();
-  });
+  }));
 });
